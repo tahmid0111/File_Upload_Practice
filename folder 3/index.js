196acd1e80039e9a29a4dd4f226874a1c995cef7
tahmid-emam <0.1tahmidemam@gmail.com>
2024-04-07 17:57:53 +0600

    just file upload with multer code added

--- folder 3/index.js
+++ folder 3/index.js
@@ -1,14 +1,6 @@
 const express = require("express");
 const multer = require("multer");
 const app = express();
-const cloudinary = require("cloudinary").v2;
-require('dotenv').config();
-
-cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.API_KEY,
-  api_secret: process.env.API_SECRET,
-});
 
 // Define storage for uploaded files
 const storage = multer.diskStorage({
@@ -25,16 +17,8 @@ const upload = multer({ storage: storage });
 
 app.post("/upload", upload.single("file"), async (req, res) => {
   try {
-    // Upload the image to Cloudinary
-    const uploadResult = await cloudinary.uploader.upload(req.file.path, {
-      folder: "users",
-    });
-    // Access uploaded image details (URL, public ID, etc.)
-    const imageUrl = uploadResult.secure_url;
-    const publicId = uploadResult.public_id;
-
     // Send a success response with the image URL
-    res.json({ message: "Image uploaded successfully!", imageUrl });
+    res.json({ message: "Image uploaded successfully!" });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Error uploading image" });
